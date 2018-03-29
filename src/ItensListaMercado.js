bd3a2a6bc7936dc18d080bfc059f0f0813f14c81
Luciano Filho <luciano.oliveira@odia.com.br>
2018-03-29 15:22:36 -0300

    Retirei o trecho de código para totalização de valores.

--- src/ItensListaMercado.js
+++ src/ItensListaMercado.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
 export class ItensListaMercado extends React.Component {
-  calcTotal(){
-    let t = '';
-    for (t in this.props.items){
-	console.log(t);    	
-	//console.log(t.total);
-        //res += t.total;
-        //console.log(res);
-    }
-  }  
-
   render() {
     return (
       <table>
@@ -34,7 +24,10 @@ export class ItensListaMercado extends React.Component {
 	        ))}
 	    </tbody>
 	    <tfoot>
-	        <tr><th colSpan="3">Total</th><th>{this.calcTotal}</th></tr>
+	        <tr>
+            <th colSpan="3">Total</th>
+            <th></th>
+          </tr>
 	    </tfoot>
 	  </table>    
     );
